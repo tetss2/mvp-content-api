81750f88da1102bc4c5a0f91a29f2e8404d0cb21
tetss2 <tetss1986@gmail.com>
2026-04-15 18:25:20 +0300

    Convert require statements to import syntax

--- parser.js
+++ parser.js
@@ -1,6 +1,6 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const fs = require("fs");
+import axios from "axios";
+import * as cheerio from "cheerio";
+import fs from "fs";
 
 const BASE_URL = "https://www.b17.ru";
 const PROFILE_URL = "https://www.b17.ru/dinakachaeva/";
